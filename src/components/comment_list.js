e7dd653f86b53376e8fccd659b5dd4e1ed7e3aeb
abarghaz <abarghaztarik@gmail.com>
2021-10-02 18:54:42 +0100

    Update comment_list.js

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 
 const CommentList = (props) => {
   const list = props.comments.map((comment) => (
-    <li key={comment}>{comment}</li>
+    <ul key={comment}>{comment}</ul>
   ));
   return <ul className="comment-list">{list}</ul>;
 };
